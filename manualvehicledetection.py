55122088eedb94f4c98ffde4581bfb73f1443136
seanjahaupan <seanjahaupan@gmail.com>
2017-11-23 13:11:55 -0800

    thresh

--- manualvehicledetection.py
+++ manualvehicledetection.py
@@ -469,13 +469,13 @@ print('non-car image size ', len(noncar_images))
 
 
 #Setting up variables
-color_space = 'YUV' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
-orient = 9  # HOG orientations
+color_space = 'YCrCb' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+orient = 8  # HOG orientations
 pix_per_cell = 8 # HOG pixels per cell
 cell_per_block = 2 # HOG cells per block
 hog_channel = 'ALL' # Can be 0, 1, 2, or "ALL"
 spatial_size = (16, 16) # Spatial binning dimensions
-hist_bins = 16    # Number of histogram bins
+hist_bins = 32    # Number of histogram bins
 hist_range = (0,256)
 spatial_feat = True # Spatial features on or off
 hist_feat = True # Histogram features on or off
